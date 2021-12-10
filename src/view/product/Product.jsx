63d2ffb33324df021cc35e40ed66e98da05f99e6
juliana176 <yuliana28074@gmail.com>
2021-12-09 22:27:08 -0500

    img complet

--- src/view/product/Product.jsx
+++ src/view/product/Product.jsx
@@ -14,8 +14,7 @@ export default function Product({
         <div className="product-data">
           <p className="product-price">{price}</p>
           <p className="product-description">{description}</p>
-
-        </div>
+          </div>
       </div>
     </div>
   );
